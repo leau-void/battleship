8526a24a372c8583ea2235b0ae70b817900bf5e2
leau-void <lpierra@gmail.com>
2021-09-24 16:21:59 -0300

    addboat func works, need to check if overlaps with other boat

--- src/gameboard/gameboardFactory.js
+++ src/gameboard/gameboardFactory.js
@@ -2,8 +2,44 @@ export default () => {
   const rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
   const ships = [];
+
+  const checkFits = (array, startIndex, length) => startIndex + length <= array.length - 1;
+
+  const findStartIndex = (array, value) => array.findIndex((curr) => curr === value);
+
+  const getTargetArray = (ship) => (ship.isHorizontal ? columns : rows);
+
+  const getRelPositions = ({ length, targetArray, start }) => {
+    const startIndex = findStartIndex(targetArray, start);
+    return [...targetArray].splice(startIndex, length);
+  };
+
+  const getTargetArrayAndStart = ({ ship, row, column }) => {
+    const targetArray = getTargetArray(ship);
+    const start = targetArray === rows ? row : column;
+    return { targetArray, start };
+  };
+
+  const getAbsPositions = ({ ship, row, column }) => {
+    const { targetArray, start } = getTargetArrayAndStart({ ship, row, column });
+    return getRelPositions({ length: ship.length, targetArray, start }).map((pos) => {
+      if (targetArray === rows) return `${column}${pos}`;
+      return `${pos}${row}`;
+    });
+  };
+
+  const checkPlace = function ({ ship, row, column, pos }) {
+    const posArray = pos || getAbsPositions({ ship, row, column });
+    if (posArray.length === ship.length) return true;
+  };
+
   const placeShip = function placeShipBoard({ ship, row, column }) {
-    this.ships.push(ship);
+    const pos = getAbsPositions({ ship, row, column });
+    const canPlace = checkPlace({ ship, row, column, pos });
+    if (!canPlace) return;
+    //  checkFits(targetArray, findStartIndex(targetArray, start), ship.length)
+
+    ships.push({ ship, row, column, pos });
   };
   return { rows, columns, ships, placeShip };
 };
--- src/gameboard/gameboardFactory.test.js
+++ src/gameboard/gameboardFactory.test.js
@@ -1,23 +1,60 @@
 import gameboardFactory from './gameboardFactory';
 import wrapShip from '../ships/wrapShipFactory';
-/* global test expect */
+/* global test expect beforeEach */
 
-test('placeShip', () => {
-  const testBoard = gameboardFactory();
+const testBoard = gameboardFactory();
 
+test('no fit column', () => {
   expect(testBoard.rows).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   expect(testBoard.columns).toEqual(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']);
 
   testBoard.placeShip({
     ship: wrapShip.cruiser({ isHorizontal: true }),
-    row: 'a',
-    column: 1,
+    row: 1,
+    column: 'i',
   });
-  expect(testBoard.ships).toEqual([
-    {
-      ship: wrapShip.cruiser({ isHorizontal: true }),
-      column: 1,
-      row: ['a', 'b', 'c'],
-    },
-  ]);
+  expect(testBoard.ships).toEqual([]);
+});
+
+test('no fit row', () => {
+  testBoard.placeShip({
+    ship: wrapShip.cruiser({ isHorizontal: false }),
+    row: 9,
+    column: 'a',
+  });
+  expect(testBoard.ships).toEqual([]);
+});
+
+test('placeShip horiz', () => {
+  const ship = wrapShip.cruiser({ isHorizontal: true });
+  testBoard.placeShip({
+    ship,
+    row: 1,
+    column: 'a',
+  });
+  const desiredObj = {
+    ship,
+    row: 1,
+    column: 'a',
+    pos: ['a1', 'b1', 'c1'],
+  };
+
+  expect(testBoard.ships[0]).toMatchObject(desiredObj);
+});
+
+test('placeShip vert', () => {
+  const ship = wrapShip.battleship({ isHorizontal: false });
+  testBoard.placeShip({
+    ship,
+    row: 3,
+    column: 'd',
+  });
+  const desiredObj = {
+    ship,
+    row: 3,
+    column: 'd',
+    pos: ['d3', 'd4', 'd5', 'd6'],
+  };
+
+  expect(testBoard.ships[1]).toMatchObject(desiredObj);
 });
